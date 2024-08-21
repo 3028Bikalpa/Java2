9c42edf3372ad3fa392070377538cd31f2134b36
3028Bikalpa <3028bikalpa@gmail.com>
2024-08-21 22:09:12 +0800

    basics

---
+++ typecastconvert.java
@@ -0,0 +1,83 @@
+
+import java.util.Scanner;
+
+
+public class typecastconvert {
+    // public static void mp(Object line){
+    //     System.out.println(line);
+    // }
+    // public static void kp(String s){ //cannot use this because the thing that we're using isline and notstring
+    //     System.out.println(s); 
+    // } 
+    public static void main(String[] args) {
+        
+        // int iNum = 10; //int = 32 bit
+        // long lNum = iNum; //long = 64 bit
+        // double dNum = lNum; //double = 64 bit
+        // System.out.println(dNum);
+
+        // double dNum = 100.95;
+        // long lNum = (long) dNum; //64 bit to 64 bit
+        // int iNum = (int) lNum; //64 bit to 32 bit type casting
+        // System.out.println("Double value: "+dNum);
+        // System.out.println("long value: "+lNum);
+
+        // char ch = 'c';
+        // int num = 88;
+        // ch = (char) num; //need to put (char) here becauseit is the conversion from 32 bit to 16 bit
+        // System.out.println(ch);
+
+        // int i = 10;
+        // float f= 9.9f;
+        // String s = i>f?"yes":"no";
+        // System.out.println("i is bigger than f? "+s);
+
+        // String myName = "Jialin Wang";
+        // String courseTitle = "Intro to Programming";
+        // System.out.println(courseTitle); //Takes the words after the space as well
+
+        // String s1 = "The title of the course is: ";
+        // String s2 = s1+courseTitle; //concatenation: adding texts using the '+' sign
+        // System.out.println(s2);
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the sides of the triangle");
+        double a = sc.nextDouble();
+        double b = sc.nextDouble();
+        double c = sc.nextDouble();
+        if(((a+b)<c)||((b+c)<a)||(a+c)<b){
+            System.out.println("This sort of triangle does not exist");
+        }else{
+        double s = (a+b+c)/2;
+        double area = Math.sqrt(s*(s-a)*(s-b)*(s-c));
+        System.out.println("The are of the triangle is "+ area);
+        }
+        //string
+        // String name = "Bikki";
+        // System.out.println(name);
+        // String greeting = "Hi! How're you doing?";
+        // String greetingname = name + ","+greeting;
+        // System.out.println(greetingname);
+        // out.println("Hi! This is the sample for the use of function of import in the very first line");
+        // mp("mp:This is the demo for the function that we made to make printing job easier");
+        // kp("kp"); //But cannot be used for line because it is for strings
+
+
+        // int mo = (int)5.1%2;
+        // System.out.println(mo);
+        // int a = 2;
+        // System.out.println(a++);
+        // System.out.println(a);
+        // System.out.println(++a);
+
+        // try {
+        //     char c =(char) System.in.read();
+        //     int n = System.in.read();
+        //     System.out.println(c);
+        //     System.out.println("The input is "+n);
+        // } catch (Exception e) {
+        //     //here we take action when the exception occurs
+        //     System.out.println("!!!There seems to be an error!!!");
+        // }
+    }
+}
