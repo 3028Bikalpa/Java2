323e882782a0f998860335e3b71906d8dc544cde
3028Bikalpa <3028bikalpa@gmail.com>
2024-08-28 22:30:41 +0800

    basics

--- roughtest.java
+++ roughtest.java
@@ -1,3 +1,7 @@
+
+import java.util.Scanner;
+
+
 public class roughtest {
     public static void main(String[] args) {
         // // char a = 'a';
@@ -12,13 +16,39 @@ public class roughtest {
         // System.out.println(a[2]);
         // System.out.println(b[2]);
 
-        int[][] my2DArray;
-        my2DArray = new int[3][];
-        my2DArray[0] = new int[4];
-        my2DArray[1]=new int[4];
-        my2DArray[2]=new int[4];
+        // int[][] my2DArray;
+        // my2DArray = new int[3][];
+        // my2DArray[0] = new int[4];
+        // my2DArray[1]=new int[4];
+        // my2DArray[2]=new int[4];
 
-        System.out.println(my2DArray);
-        System.out.println(my2DArray[1]);
+        // System.out.println(my2DArray);
+        // System.out.println(my2DArray[1]);
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Which number of day do you want to print?");
+        int num = sc.nextInt();
+        switch(num){
+            case 1:
+            System.out.println("Monday");
+            break;
+            case 2:
+            System.out.println("Tuesday");
+            break;
+            case 3:
+            System.out.println("Wednesday");
+            break;
+            case 4:
+            System.out.println("Thursday");
+            break;
+            case 5:
+            System.out.println("Friday");
+            break;
+            case 6:
+            System.out.println("Saturday");
+            break;
+            case 7:
+            System.out.println("Sunday");
+            break;
+        }
     }
 }
