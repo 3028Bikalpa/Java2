0de2c20c314ed9161694cc34838e59336e8f9169
3028Bikalpa <3028bikalpa@gmail.com>
2024-09-03 10:13:12 +0800

    exception

--- MultiInher_Interface.java
+++ MultiInher_Interface.java
@@ -3,6 +3,32 @@ public class MultiInher_Interface {
         //Child c = new Child(); this will call default constructor
         Child c = new Child("Jingrui");
         System.out.println("The name is: "+c.name);
+
+        Whale w = new Whale();
+        w.giveBirth();
+        w.swim();
+
+        System.out.println(w.name);
+    }
+}
+
+interface Mammal{
+    public void giveBirth();
+    final static String name = "Cheng Binghan";
+
+    // public void produceMilk();
+}
+
+interface MarineAni{
+    public void swim();
+}
+
+class Whale implements Mammal, MarineAni{
+    public void giveBirth(){
+        System.out.println("Hi thi is giveBirth.");
+    }
+    public void swim(){
+        System.out.println("Hi, this is swim");
     }
 }
 
