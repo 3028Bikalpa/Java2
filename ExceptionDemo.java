4ec02b0e0ef07c98d4dce945277b9aa4f8ab6fa4
3028Bikalpa <3028bikalpa@gmail.com>
2024-09-04 11:02:25 +0800

    exception

--- ExceptionDemo.java
+++ ExceptionDemo.java
@@ -1,5 +1,8 @@
+
+import java.io.FileNotFoundException;
+
 public class ExceptionDemo {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
         String myName="Toyota";
         //syntax error
         int i = (int)(10*34.5);
@@ -8,8 +11,21 @@ public class ExceptionDemo {
         // int j = 50/0; //exception not caught
 
         //exception handling
+
+        //null pointer exception
+        String s = "Binhan Chen";
+        char c = s.charAt(0);
+
+        Student s1 = new Student();
+        s1.name ="Dongyu";
+        Student[] stuArray = new Student[40];
+        stuArray[1] = new Student();
+        stuArray[1].name = "Muyu";
+
         try {
-            int j = 50/0;
+            // int j = 50/0;
+            double d = 50/0.0;
+            System.out.println("d = "+d);
         } 
         //if exception occurs from above
         catch (ArithmeticException e) {
@@ -35,10 +51,33 @@ public class ExceptionDemo {
         }
         //We can use multiple catch in order to catch multiple types of exceptions in the same program, but the thing is, once the first error is found the compiler
         // breaks from that sequence and goes to catch, which means once the first error is found then it will directly go to catch rather than checking for other exceptions
-        
+
+        //demo in exception throwing
+        // try {
+        //     Scanner in = new Scanner(new File("Feiyang.txt"));
+        // } catch (FileNotFoundException e) {
+        //     System.out.println("我不认识那个人");
+        // }
+        Scanner in = new Scanner(new File("Feiyang.txt"));
     }
 }
 
+class Student{
+    String name;
+    String reactToGrade(int points) throws HeartBrokenException{
+        if(points > 97) return "Ecstatic";
+        else if (points > 75) return "Okay";
+        else if(points>60) return "Depressed!!!";
+        // else return "Quit!";
+        throw new HeartBrokenException("I am DONE");
+    }
+}
+
+class HeartBrokenException extends Exception{
+    HeartBrokenException (String msg){
+        super(msg);
+    }
+}
 interface Anar1{
     void NotInYesterday();
     String placeToGo="Shanghai";
