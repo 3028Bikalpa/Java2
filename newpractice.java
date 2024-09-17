a728319ea5ff7fada6d5b0894f0c2f3fd7b51c87
3028Bikalpa <3028bikalpa@gmail.com>
2024-09-17 23:52:37 +0800

    basics

--- newpractice.java
+++ newpractice.java
@@ -1,9 +1,17 @@
 public class newpractice {
     public static void main(String[] args) {
-        int a = 100;
-        int b = 10_0;
-        int c = 23_8;
-        int d = a+b+c;
-        System.out.println(d);
+        // int a = 100;
+        // int b = 10_0;
+        // int c = 23_8;
+        // int d = a+b+c;
+        // System.out.println(d);
+        // int result = ++a;
+        // int result2 = a++;
+        // System.out.println(result);
+        // System.out.println(result2);
+
+        int num = 7;
+        int result = num++;
+        System.out.println(result);
     }
 }
